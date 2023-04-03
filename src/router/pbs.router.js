c46f006bf4920c4792f5d450914ce7abebdacb30
FabricioCruzz <fabriciocruzz77@gmail.com>
2023-04-02 23:40:26 -0300

    Criando schema no prisma - reg_person table

--- src/router/pbs.router.js
+++ src/router/pbs.router.js
@@ -10,3 +10,8 @@ router.put("/:id", controller.update);
 router.delete("/:id", controller.remove);
 
 module.exports = router;
+
+/*
+CONTINUAR SEGUINDO O TUTORIAL DO PRISMA PARA FAZER MIGRATION
+https://www.prisma.io/docs/getting-started/setup-prisma/start-from-scratch/relational-databases/using-prisma-migrate-node-postgres
+*/
